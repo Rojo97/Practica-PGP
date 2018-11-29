3225872e4fbd068304dda0687f708007131a25f8
Ivan <ivan.gonzalez.rincon>
2018-11-29 17:54:49 +0100

    modificado el sql de la bd

--- geapros.sql
+++ geapros.sql
@@ -1,11 +1,17 @@
-DROP TABLE Proyecto;
-DROP TABLE Usuario;
-DROP TABLE Actividad;
+DROP TABLE TipoUsuario;
+DROP TABLE EstadoActividad;
+DROP TABLE EstadoProyecto;
 DROP TABLE Participacion;
+DROP TABLE Predecesora;
+DROP TABLE Actividad;
+DROP TABLE Usuario;
+DROP TABLE Proyecto;
 
-CREATE TABLE Proyecto {
-
+CREATE TABLE Proyecto(
     nombre CHAR(50),
-    fecha_inicial DATE
-
-};
+    fecha_inicial DATE,
+    estado INTEGER,
+    presupuesto REAL,
+    informeDeSeguimiento CHAR(50),
+    resumen CHAR(50)
+);
