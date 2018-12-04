164e2722296266315b7ef1afda6db85448ec3334
Rojo97 <victorrojo1997@gmail.com>
2018-12-04 14:27:42 +0100

    Test de conexion a backend

--- codigo/src/pagesScript/CrearUsuario.js
+++ codigo/src/pagesScript/CrearUsuario.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 
 export default class CrearUsuario extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            login:'Rojo',
+            password:'soylapuñeterapass',
+            dni:'soyeldni',
+            nombre:'Corde',
+            apellido1:'benito',
+            apellido2:'camelas',
+            fechaNacimiento:'2018-12-3',
+            tipo:'0',
+            categoria:'3',
+        }
+    }
+
+    addUser = _ => {
+        fetch(`http://localhost:8080/usuario/post?nickUsuario=${this.state.login}&contrasenia=${this.state.password}&dni=${this.state.dni}&nombre=${this.state.nombre}&apellido1=${this.state.apellido1}&apellido2=${this.state.apellido2}&fechaNacimiento=${this.state.fechaNacimiento}&tipoUsuario=${this.state.tipo}&categoriaUsuario=${this.state.categoria}`)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+    }
+
     render() {
         return (
             <div className="content-wrapper">
@@ -23,16 +44,6 @@ export default class CrearUsuario extends Component {
                                                     <label for="exampleInputPassword1">Password</label>
                                                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
                                                 </div>
-                                                <div class="form-group">
-                                                    <label>Select</label>
-                                                    <select class="form-control">
-                                                        <option>option 1</option>
-                                                        <option>option 2</option>
-                                                        <option>option 3</option>
-                                                        <option>option 4</option>
-                                                        <option>option 5</option>
-                                                    </select>
-                                                </div>
                                             </column>
                                             <column>
                                                 <div class="form-group">
@@ -48,21 +59,40 @@ export default class CrearUsuario extends Component {
                                                     <input class="form-control" type="text" placeholder="Default input" />
                                                 </div>
                                                 <div class="form-group">
-                                                    <label>Date masks:</label>
-
-                                                    <div class="input-group">
+                                                    <label>Date:</label>
+                                                    <div class="input-group date">
                                                         <div class="input-group-addon">
                                                             <i class="fa fa-calendar"></i>
                                                         </div>
-                                                        <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask />
+                                                        <input type="text" class="form-control pull-right" id="datepicker"/>
                                                     </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Select</label>
+                                                    <select class="form-control">
+                                                        <option>option 1</option>
+                                                        <option>option 2</option>
+                                                        <option>option 3</option>
+                                                        <option>option 4</option>
+                                                        <option>option 5</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Select</label>
+                                                    <select class="form-control">
+                                                        <option>option 1</option>
+                                                        <option>option 2</option>
+                                                        <option>option 3</option>
+                                                        <option>option 4</option>
+                                                        <option>option 5</option>
+                                                    </select>
                                                 </div>
                                             </column>
                                         </row>
                                     </div>
                                     <div className="box-footer">
                                         <div>
-                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                            <button type="submit" class="btn btn-primary" onClick={this.addUser}>Submit</button>
                                         </div>
                                     </div>
                                 </form>
