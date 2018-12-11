9c3c42c8aaf7f75064f1e8bab08c64a616c771b0
Ivan <ivan.gonzalez.rincon>
2018-12-11 13:45:34 +0100

    Añadidos cambios para comprobar nombre de Proyecto

--- rest-service/app/proyectos/route.js
+++ rest-service/app/proyectos/route.js
@@ -1,7 +1,5 @@
 function init(app, dbPool, db) {
 
-    //ToDo: Comprobar que no exista el nombre de un proyecto cuando se va a crear
-
     app.post('/api/proyecto', (req, res) => {
         console.log(req.body);
         var args = [req.body.nombreProyecto, req.body.resumen];
@@ -14,10 +12,38 @@ function init(app, dbPool, db) {
                 response.status(201).json({});
             }
         };
-
-        db.execQuery(dbPool, query, args, onResults, res);
-        db.execQuery(dbPool, query2, args2, onResults, res);
+        comprobarNombreProyecto(req.body.nombreProyecto,function(count){
+            if (count == 0) {
+                console.log("Entro bien");
+                db.execQuery(dbPool, query, args, onResults, res);
+                //db.execQuery(dbPool, query2, args2, onResults, res);
+            } else {
+                console.log("El proyecto ya existe");
+            }
+        });
+        
     })
+
+    //ToDo: Comprobar que no exista el nombre de un proyecto cuando se va a crear
+    function comprobarNombreProyecto(nombreProyecto, callback) {
+        //La consulta devuelve un conteo con el nombre del proyecto introducido. Si es distinto de 0, el proyecto no puede crearse.
+        var args = [nombreProyecto];
+        var query = db.querys.proyectos.countProyectosByNombre;
+        dbPool.getConnection(function (err, connection) {
+            if (err) {
+                console.log("Error en la conexión");
+            } else {
+                connection.query(query, args, function (error, results, fields) {
+                    connection.release();
+                    if (error) {
+                        console.log("Error");
+                    } else {
+                        callback(results[0].numeroProyectos);
+                    }
+                })
+            }
+        });
+    }
 }
 
 module.exports = init;
