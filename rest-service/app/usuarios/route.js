f9ebc507d5c980a4d1ae5079292790d189b9a285
antroma <antonio.roman@alumnos.uva.es>
2018-12-26 21:46:24 +0100

    Añadido recurso para obtener los proyectos de un usuario dado, a falta de consultas

--- rest-service/app/usuarios/route.js
+++ rest-service/app/usuarios/route.js
@@ -1,6 +1,6 @@
 function init(app, dbPool, db) {
     app.get('/api/usuario', (req, res) => {
-        if(req.query.selectableAsJefe){
+        if(req.query.selectableAsJefe == 1){
             var query = db.querys.usuarios.getJefesSinProyecto;
         }else{
             var query = db.querys.usuarios.getUsuarios;
@@ -27,6 +27,34 @@ function init(app, dbPool, db) {
         const nickUsuario = req.params.nickUsuario;
         var query = db.querys.usuarios.getUsuariosByNick;
         console.log(nickUsuario);
+        var args = [nickUsuario];
+
+        function onResults(error, results, response) {
+            if (!error) {
+                if (results.length == 0) {
+                    response.sendStatus(404);
+                } else {
+                    return res.status(200).json({
+                        data: results
+                    })
+                }
+            }
+        }
+
+        db.execQuery(dbPool, query, args, onResults, res);
+    })
+
+    app.get('/api/usuario/:nickUsuario/proyectos', (req, res) => {
+        const nickUsuario = req.params.nickUsuario;
+        if(req.query.actual == 1){
+            console.log("Actuales");
+            var query = db.querys.usuarios.getProyectosActualesUsuario;
+        }else{
+            console.log("Todos");
+            var query =  db.querys.usuarios.getProyectosUsuario;
+        }
+        console.log(nickUsuario);
+        console.log(req.query.actual);
         var args = [nickUsuario];
 
         function onResults(error, results, response) {
