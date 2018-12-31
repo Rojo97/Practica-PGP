e8a3ffa3f6ef3486bcddf221207c3fed1c7d1290
Rojo97 <victorrojo1997@gmail.com>
2018-12-31 13:50:23 +0100

    Hotfix

--- codigo/src/App.js
+++ codigo/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Login from './pagesScript/Login';
 import Plantilla from './pagesScript/Plantilla';
-import CrearUsuario from './pagesScript/CrearUsuario';
-import DarAltaProyecto from './pagesScript/DarAltaProyecto';
 
 class App extends Component {
     render() {
--- codigo/src/pagesScript/Plantilla.js
+++ codigo/src/pagesScript/Plantilla.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Header from '../components/Header';
 import Sidebar from '../components/SideBar';
-import Content from '../components/Content';
 import CrearUsuario from '../components/CrearUsuario';
 import SelectProject from '../components/SelectProject';
 import ShowActivitiesDeveloper from '../components/ShowActivitiesDeveloper';
 import ShowActivitiesManager from '../components/ShowActivitiesManager';
+import DarAltaProyecto from '../components/DarAltaProyecto';
 
 class Plantilla extends Component {
   render() {
@@ -16,7 +16,7 @@ class Plantilla extends Component {
         <Sidebar />
         <Switch>
           <Route exact path="/admin/createUser" component={CrearUsuario} />
-          <Route exact path="/admin/createProject" component={Content} />
+          <Route exact path="/admin/createProject" component={DarAltaProyecto} />
           <Route exact path="/developer/selectProject" component={SelectProject}/>
           <Route exact path="/developer/project/:nombre/activities" component={ShowActivitiesDeveloper}/>
           <Route exact path="/projectManager/project/:nombre/activities" component={ShowActivitiesManager}/>
