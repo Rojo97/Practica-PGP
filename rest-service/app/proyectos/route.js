df04c3933a9a237e16b33489c57a78eeac2ea088
antroma <antonio.roman@alumnos.uva.es>
2019-01-04 00:27:44 +0100

    Recurso para obtener candidatos a formar parte de un proyecto

--- rest-service/app/proyectos/route.js
+++ rest-service/app/proyectos/route.js
@@ -24,7 +24,7 @@ function init(app, dbPool, db) {
 
     })
 
-    app.post('/api/proyecto/:nombreProyecto/cargaPlan', (req, res) => {
+    app.post('/api/proyecto/:nombreProyecto/cargaPlan', VerifyToken, (req, res) => {
 
         var nombreProyecto = req.params.nombreProyecto;
         var actividades = req.body.actividades;
@@ -35,11 +35,11 @@ function init(app, dbPool, db) {
         function onResults(error, results, response) {
             if (error) console.log(error);
         };
-                
+
         actividades.forEach(actividad => {
             args = [actividad.nombre, nombreProyecto, actividad.descripcion, actividad.duracion, actividad.rol];
             db.execQuery(dbPool, query1, args, onResults, res);
-        
+
         });
 
         actividades.forEach(actividad => {
@@ -75,7 +75,7 @@ function init(app, dbPool, db) {
 
     app.get('/api/proyecto/:nombreProyecto/participantes', VerifyToken, (req, res) => {
         const nombreProyecto = req.params.nombreProyecto;
-        
+
         if (req.query.rol) {
             var query = db.querys.proyectos.getParticipantesProyectoConRol;
             var args = [nombreProyecto, req.query.rol];
@@ -83,6 +83,29 @@ function init(app, dbPool, db) {
             var query = db.querys.proyectos.getParticipantesProyecto;
             var args = [nombreProyecto];
         }
+
+
+        function onResults(error, results, response) {
+            if (!error) {
+                if (results.length == 0) {
+                    response.sendStatus(404);
+                } else {
+                    console.log("Peticion recibida");
+                    return res.status(200).json({
+                        data: results
+                    })
+                }
+            } else { res.status(500).send('Error on the server.'); }
+        }
+
+        db.execQuery(dbPool, query, args, onResults, res);
+    })
+
+    app.get('/api/proyecto/:nombreProyecto/candidatos', VerifyToken, (req, res) => {
+        const nombreProyecto = req.params.nombreProyecto;
+
+        var query = db.querys.proyectos.getCandidatos;
+        var args = [nombreProyecto];
 
 
         function onResults(error, results, response) {
