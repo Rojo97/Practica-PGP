254235d01a9b8798002c0b35c6b47531406ec3e1
ismpere <ismaelprzm@gmail.com>
2019-01-04 13:02:08 +0100

    Arreglo el conflicto en plantilla

--- codigo/src/pagesScript/Plantilla.js
+++ codigo/src/pagesScript/Plantilla.js
@@ -7,12 +7,9 @@ import SelectProject from '../components/SelectProject';
 import ShowActivitiesDeveloper from '../components/ShowActivitiesDeveloper';
 import ShowActivitiesManager from '../components/ShowActivitiesManager';
 import DarAltaProyecto from '../components/DarAltaProyecto';
-<<<<<<< HEAD
 import AnadirPlanProyecto from '../components/AnadirPlanProyecto';
-=======
 import ActivityDetail from '../components/ActivityDetail';
 import ActivityDetailAndEdit from '../components/ActivityDetailAndEdit';
->>>>>>> origin/develop
 
 class Plantilla extends Component {
   render() {
@@ -27,11 +24,8 @@ class Plantilla extends Component {
           <Route exact path="/developer/project/:nombre/activities" component={ShowActivitiesDeveloper}/>
           <Route exact path="/developer/activity/:nombre" component={ActivityDetail}/>
           <Route exact path="/projectManager/project/:nombre/activities" component={ShowActivitiesManager}/>
-<<<<<<< HEAD
           <Route exact path="/projectManager/project/:nombre/loadProyectPlan" component={AnadirPlanProyecto}/>
-=======
           <Route exact path="/projectManager/activity/:nombre" component={ActivityDetailAndEdit}/>
->>>>>>> origin/develop
         </Switch>
       </div>
     );
