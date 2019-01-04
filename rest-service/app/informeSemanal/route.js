140559612cfc853376de8e92a76d559018ab6055
antroma <antonio.roman@alumnos.uva.es>
2019-01-04 22:45:42 +0100

    Recurso para añadir un nuevo informe semanal creado

---
+++ rest-service/app/informeSemanal/route.js
@@ -0,0 +1,20 @@
+var VerifyToken = require('../auth/VerifyToken');
+
+function init(app, dbPool, db) {
+    app.post('/api/informeSemanal', VerifyToken, (req, res) => {
+        console.log(req.body);
+        
+        var args = [req.body.nombreActividad, req.body.nombreProyecto, req.body.nickUsuario, req.body.informeTareasPersonales, req.body.horas];
+        const query = db.querys.informeSemanal.insert;
+
+        function onResults(error, results, response) {
+            if (!error) {
+                response.status(201).json({});
+            } else { res.status(500).send('Error on the server.'); }
+        };
+
+        db.execQuery(dbPool, query, args, onResults, res);
+    })
+}
+
+module.exports = init;
