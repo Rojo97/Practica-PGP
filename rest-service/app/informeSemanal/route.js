a76c1826ac1891968964695e7db283f43d80575c
antroma <antonio.roman@alumnos.uva.es>
2019-01-05 17:17:00 +0100

    Añadido recurso para actualizar un informe semanal

--- rest-service/app/informeSemanal/route.js
+++ rest-service/app/informeSemanal/route.js
@@ -2,10 +2,25 @@ var VerifyToken = require('../auth/VerifyToken');
 
 function init(app, dbPool, db) {
     app.post('/api/informeSemanal', VerifyToken, (req, res) => {
-        console.log(req.body);
 
         var args = [req.body.nombreActividad, req.body.nombreProyecto, req.body.nickUsuario, req.body.informeTareasPersonales, req.body.horas];
         const query = db.querys.informeSemanal.insert;
+
+        function onResults(error, results, response) {
+            if (!error) {
+                console.log("Informe creado");
+                response.status(201).json({});
+            } else { res.status(500).send('Error on the server.'); }
+        };
+
+        db.execQuery(dbPool, query, args, onResults, res);
+    })
+
+    app.put('/api/informeSemanal', VerifyToken, (req, res) => {
+        console.log(req.body);
+
+        var args = [re.body.estado, req.body.nombreActividad, req.body.nickUsuario, req.body.nombreProyecto];
+        const query = db.querys.informeSemanal.updateEstado;
 
         function onResults(error, results, response) {
             if (!error) {
