70df89bd7efdd44f88488c1683f9ec3ac5135acc
Rojo97 <victorrojo1997@gmail.com>
2019-01-06 20:16:10 +0100

    Primer informe generado

--- codigo/src/components/ProjectDetailsManager.js
+++ codigo/src/components/ProjectDetailsManager.js
@@ -5,13 +5,17 @@ export default class ProjectDetailsManager extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            proyecto: [],
-            estado: "ver",
-            fechaFin: "",
-            informeTemporal: "",
-            resumen: "",
-            tipoInforme:"",
-            estadoProyecto: "",
+            proyecto: [], //Proyecto
+            estado: "ver", //Estado principal (ver, editar o informes)
+            fechaInicioIntervalo: "", //Fechas del intervalo para los informes que lo necesiten
+            fechaFinIntervalo: "",
+            informeTemporal: "", //Segimiento temporal del proyecto
+            resumen: "", //Resumen del proyecto
+            tipoInforme: "", //Tipo de informe que se esta mostrando
+            tipoInformeTmp: "", //Informe marcado en el select
+            subEstado: "", //Esta a 1 cuando se va a mostar un inorme (Se muestra segunda parte)
+            estadoProyecto: "", //Estado proyecto
+            datosInforme: [], //Variable donde guardamos el fetch de los datos del proyecto
             estados: ["En curso", "Finalizado", "Cerrado", "Aprobado"],
             roles: ["Administrador", "Jefe de proyecto", "Analista", "Diseñador, Analista-Programador o Responsable del equipo de pruebas", "Programador o probador"],
         }
@@ -103,10 +107,36 @@ export default class ProjectDetailsManager extends Component {
         console.log(this.state);
     }
 
+    seleccionarInforme = () => {
+        this.setState({ subEstado: 1 });
+        this.setState({ tipoInforme: this.state.tipoInformeTmp });
+        //Aqui en caso de que el informe necesita el fetch se llama a la funcion del fetch
+    }
+
+    informeTrabajadores = () => {
+        const proyecto = this.props.match.params.proyecto;
+
+        fetch(`http://virtual.lab.inf.uva.es:27014/api/proyecto/${proyecto}/actividades/${this.state.fechaInicioIntervalo}/${this.state.fechaFinIntervalo}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-access-token': window.sessionStorage.getItem('token')
+            }
+        }).then(function (response) {
+            console.log(response);
+            return response.json()
+        })
+            .then(responseJson => this.setState({ datosInforme: responseJson.data }))
+            .catch(function (data) { console.log(data) });
+    }
+
     render() {
         let botones;
         let edicion;
         let informes;
+        let intervalo;
+        let datos = '';
 
         if (this.state.estado === "ver") {
             botones =
@@ -167,7 +197,7 @@ export default class ProjectDetailsManager extends Component {
                     <button className="btn btn-info pull-right" onClick={this.activeEdit}>Editar</button>
                 </div>;
             edicion = "";
-            informes = <form>
+            informes =
                 <div className="box ">
                     <div className="box-header with-border">
                         <h3 className="box-title">Generar informe</h3>
@@ -175,17 +205,62 @@ export default class ProjectDetailsManager extends Component {
                     <div className="box-body">
                         <div className="form-group">
                             <label>Tipo de informe:</label>
-                            <select className="form-control" name="tipoInforme" value={this.state.tipoInforme} onChange={this.handleInputChange} >
+                            <select className="form-control" name="tipoInformeTmp" value={this.state.tipoInformeTmp} onChange={this.handleInputChange} >
                                 <option disabled value=''> -- Seleccione informe -- </option>
+                                <option value="trabajadoresActividades">Trabajadores de las actividades</option>
                                 <option value="WIP">WIP</option> {/*TODO AÑADIR TIPOS DE INFORME */}
                             </select>
                         </div>
                     </div>
                     <div className="box-footer">
-                        <button className="btn btn-info pull-right">Generar</button>
+                        <button className="btn btn-info pull-right" onClick={this.seleccionarInforme}>Generar</button>
                     </div>
-                </div>
-            </form>;
+                </div>;
+        }
+
+        if (this.state.subEstado == 1) {
+            if (this.state.tipoInforme == "trabajadoresActividades") {
+                intervalo = <div className="box ">
+                    <div className="box-header with-border">
+                        <h3 className="box-title">Periodo</h3>
+                    </div>
+                    <div className="box-body">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Desde:</label>
+                                <div className="input-group date">
+                                    <div className="input-group-addon">
+                                        <i className="fa fa-calendar"></i>
+                                    </div>
+                                    <input type="date" className="form-control pull-right" name="fechaInicioIntervalo" value={this.state.fechaInicioIntervalo} onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Hasta:</label>
+                                <div className="input-group date">
+                                    <div className="input-group-addon">
+                                        <i className="fa fa-calendar"></i>
+                                    </div>
+                                    <input type="date" className="form-control pull-right" name="fechaFinIntervalo" value={this.state.fechaFinIntervalo} onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box-footer">
+                        <button type="submit" className="btn btn-info pull-right" onClick={this.informeTrabajadores}>Buscar</button>
+                    </div>
+                </div>;
+                datos =
+                    <div className="box">
+                        <div className="box-body">
+                            {this.state.datosInforme.map(datos => (
+                                <h3>Actividad: {datos.nombreActividad}, Participante: {datos.nickUsuario}</h3>
+                            ))}
+                        </div>
+                    </div>
+            }
         }
 
         return (
@@ -218,10 +293,12 @@ export default class ProjectDetailsManager extends Component {
                             </div>
                             {edicion}
                             {informes}
+                            {intervalo}
+                            {datos}
                         </div>
                     </div>
                 </section>
-            </div>
+            </div >
         )
     }
 }
